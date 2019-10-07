beaa6b8cbdce2601da7dbdb281e7ec2a534b1491
Swen Wenzel <swen.wenzel@real-digital.de>
2019-10-07 13:42:19 +0200

    improve docs and refactor pykafka.sasl_authenticators

--- pykafka/sasl_authenticators.py
+++ pykafka/sasl_authenticators.py
@@ -8,8 +8,14 @@ from uuid import uuid4
 import six
 
 from .exceptions import AuthenticationException, ERROR_CODES, UnsupportedSaslMechanism
-from .protocol import (SaslHandshakeRequest, SaslHandshakeResponse, ApiVersionsRequest, ApiVersionsResponse,
-    SaslAuthenticateRequest, SaslAuthenticateResponse)
+from .protocol import (
+    SaslHandshakeRequest,
+    SaslHandshakeResponse,
+    ApiVersionsRequest,
+    ApiVersionsResponse,
+    SaslAuthenticateRequest,
+    SaslAuthenticateResponse,
+)
 
 log = logging.getLogger(__name__)
 
@@ -22,8 +28,19 @@ else:
         return bytes(lb ^ rb for lb, rb in zip(left, right))
 
 
-class FakeRequest:
+class BytesWrapper:
+    """
+    Class that implements :meth:`get_bytes` and wraps some payload so it can be used for
+    :meth:`connection.BrokerConnection.request` during legacy sasl authentication sequence.
+    """
+
     def __init__(self, payload):
+        """
+        Create a new FakeRequest.
+
+        :param payload: The payload to wrap
+        :type payload: bytes
+        """
         self.payload = payload
 
     def get_bytes(self):
@@ -40,6 +57,7 @@ class BaseAuthenticator:
         :meth:`BaseAuthenticator.send_and_receive` to send and receive the byte strings necessary to authenticate
         with the broker.
     """
+
     MAX_AUTH_VERSION = 1
     MAX_HANDSHAKE_VERSION = 1
 
@@ -61,22 +79,39 @@ class BaseAuthenticator:
         self._broker_connection = None
 
     def get_rd_kafka_opts(self):
+        """
+        Creates the config entries necessary for librdkafka to successfully authenticate with the broker.
+
+        :return: Dictionary to enrich config for librdkafka based consumers and producers.
+        """
         raise NotImplementedError()
 
     def authenticate(self, broker_connection):
+        """
+        Runs the authentication sequence on the given broker connection.
+
+        .. warning::
+           This is not thread safe!
+
+        :param broker_connection: The broker connection to authenticate with.
+        :type broker_connection: :class:`pykafka.connection.BrokerConnection`
+        """
         self._broker_connection = broker_connection
         if self.handshake_version is None:
-            self.fetch_api_versions()
+            self._fetch_api_versions()
         log.debug(
             "Authenticating to {}:{} using mechanism {}.".format(
                 self._broker_connection.host, self._broker_connection.port, self.mechanism
             )
         )
-        self.initialize_authentication()
+        self._initialize_authentication()
         self.exchange_tokens()
         log.debug("Authentication successful.")
 
-    def initialize_authentication(self):
+    def _initialize_authentication(self):
+        """
+        Initializes the authentication sequence.
+        """
         self._broker_connection.request(SaslHandshakeRequest.get_versions()[self.handshake_version](self.mechanism))
         response = SaslHandshakeResponse.get_versions()[self.handshake_version](self._broker_connection.response())
         if response.error_code != 0:
@@ -86,16 +121,30 @@ class BaseAuthenticator:
             raise ERROR_CODES[response.error_code]("Authentication Handshake failed")
 
     def exchange_tokens(self):
+        """
+        Runs the authentication sequence. Implementation varies among SASL mechanism and has to be supplied by
+        subclasses. See also :meth:`PlainAuthenticator.exchange_tokens` or :meth:`ScramAuthenticator.exchange_tokens`
+        for exemplary implementations.
+        """
         raise NotImplementedError()
 
     def send_and_receive(self, token):
+        """
+        Sends the given token to the broker and receives the brokers response.
+        This will automatically use the appropriate mechanism to do so.
+        I.e. use SaslAuthenticateRequest if the server supports it or just send the bytes directly if it doesn't.
+
+        :param token: The token to be sent to the broker.
+        :type token: bytes
+        :return: bytes, the servers response
+        """
         self._send_token(token)
         return self._receive_token()
 
     def _send_token(self, token):
         log.debug("Seding auth token")
         if self.handshake_version == 0:
-            req = FakeRequest(token)
+            req = BytesWrapper(token)
         else:
             req = SaslAuthenticateRequest.get_versions()[self.auth_version](token)
         self._broker_connection.request(req)
@@ -111,18 +160,27 @@ class BaseAuthenticator:
             raise ERROR_CODES[response.error_code](response.error_message)
         return response.auth_bytes
 
-    def fetch_api_versions(self):
+    def _fetch_api_versions(self):
+        """
+        The api version request can be run without authentication in order to determine which authentication api
+        versions to use. That's what this method does.
+        """
         log.debug("Fetch SASL authentication api versions.")
         self._broker_connection.request(ApiVersionsRequest())
         response = ApiVersionsResponse(self._broker_connection.response())
+
         self.handshake_version = response.api_versions[SaslHandshakeRequest.API_KEY].max
         self.auth_version = response.api_versions.get(SaslAuthenticateRequest.API_KEY, None)
+
         self.handshake_version = min(self.MAX_HANDSHAKE_VERSION, self.handshake_version)
         if self.auth_version is not None:
             self.auth_version = min(self.auth_version.max, self.MAX_AUTH_VERSION)
-        log.debug("Determinded handshake api version {} and authenticate api version {}".format(
-            self.handshake_version, self.auth_version
-        ))
+        log.debug(
+            "Determinded handshake api version {} and authenticate api version {}".format(
+                self.handshake_version, self.auth_version
+            )
+        )
+
 
 class ScramAuthenticator(BaseAuthenticator):
     """
@@ -160,13 +218,24 @@ class ScramAuthenticator(BaseAuthenticator):
         self.server_key = None
         self.server_signature = None
 
-    def first_message(self):
+    def client_first_message(self):
+        """
+        Create and return the client first message. This will also reset all internal variables.
+        :return: str, the client first message
+        """
         self.nonce = str(uuid4()).replace("-", "")
         client_first_bare = "n={},r={}".format(self.user, self.nonce)
         self.auth_message = client_first_bare
         return "n,," + client_first_bare
 
     def process_server_first_message(self, server_first_message):
+        """
+        Parse and process server first message, this will extract all necessary information from the server's first
+        response such as iteration count or salt and use it to prepare the client final message.
+
+        :param server_first_message: The first message sent by the server
+        :type server_first_message: str
+        """
         self.auth_message += "," + server_first_message
         params = dict(pair.split("=", 1) for pair in server_first_message.split(","))
         server_nonce = params["r"]
@@ -177,25 +246,45 @@ class ScramAuthenticator(BaseAuthenticator):
 
         salt = base64.b64decode(params["s"].encode())
         iterations = int(params["i"])
-        self.create_salted_password(salt, iterations)
+        self._create_salted_password(salt, iterations)
 
-        self.client_key = self.hmac(self.salted_password, b"Client Key")
+        self.client_key = self._hmac(self.salted_password, b"Client Key")
         self.stored_key = self.hashfunc(self.client_key).digest()
-        self.client_signature = self.hmac(self.stored_key, self.auth_message.encode())
+        self.client_signature = self._hmac(self.stored_key, self.auth_message.encode())
         self.client_proof = xor_bytes(self.client_key, self.client_signature)
-        self.server_key = self.hmac(self.salted_password, b"Server Key")
-        self.server_signature = self.hmac(self.server_key, self.auth_message.encode())
+        self.server_key = self._hmac(self.salted_password, b"Server Key")
+        self.server_signature = self._hmac(self.server_key, self.auth_message.encode())
 
-    def hmac(self, key, msg):
+    def _hmac(self, key, msg):
+        """
+        Run the hmac algorithm on `key` and `msg` using the appropriate digest method for the configures scram
+        mechanism.
+        :param key: The key for the hmac algorithm
+        :type key: bytes
+        :param msg: The message for the hmac algorithm
+        :type msg: bytes
+        :return: bytes, the result of applying hmac on `key` and `msg`
+        """
         return hmac.new(key, msg, digestmod=self.hashfunc).digest()
 
-    def create_salted_password(self, salt, iterations):
+    def _create_salted_password(self, salt, iterations):
         self.salted_password = hashlib.pbkdf2_hmac(self.hashname, self.password, salt, iterations)
 
-    def final_message(self):
+    def client_final_message(self):
+        """
+        Create and return the client final message.
+        :return: str, the client final message
+        """
         return "c=biws,r={},p={}".format(self.nonce, base64.b64encode(self.client_proof).decode())
 
     def process_server_final_message(self, server_final_message):
+        """
+        Parse and process server final message. This will run validation on the server's response to make sure that
+        everything is all right.
+
+        :param server_final_message: The first message sent by the server
+        :type server_final_message: str
+        """
         params = dict(pair.split("=", 1) for pair in server_final_message.split(","))
         if self.server_signature != base64.b64decode(params["v"].encode()):
             raise AuthenticationException("Server sent wrong signature!")
@@ -209,11 +298,11 @@ class ScramAuthenticator(BaseAuthenticator):
         }
 
     def exchange_tokens(self):
-        client_first = self.first_message()
+        client_first = self.client_first_message()
         server_first = self.send_and_receive(client_first.encode()).decode()
         self.process_server_first_message(server_first)
 
-        client_final = self.final_message()
+        client_final = self.client_final_message()
         server_final = self.send_and_receive(client_final.encode()).decode()
         self.process_server_final_message(server_final)
 
