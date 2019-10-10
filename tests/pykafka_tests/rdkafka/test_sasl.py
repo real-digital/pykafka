12399ce96031ae16e351b6bc31636d451ef298b6
Swen Wenzel <swen.wenzel@real-digital.de>
2019-10-10 10:57:37 +0200

    skip sasl rdkafka tests if it's not installed

--- tests/pykafka_tests/rdkafka/test_sasl.py
+++ tests/pykafka_tests/rdkafka/test_sasl.py
@@ -1,8 +1,15 @@
+import pytest
 from uuid import uuid4
 
 from pykafka import KafkaClient
+try:
+    from pykafka.rdkafka import _rd_kafka
+    RDKAFKA = True
+except ImportError:
+    RDKAFKA = False  # C extension not built
 
 
+@pytest.mark.skipif(not RDKAFKA, reason="C extension for librdkafka not built.")
 def test_sasl_roundtrip_rdkafka(sasl_kafka, authenticator, kafka_version):
     client = KafkaClient(sasl_kafka.brokers_sasl, sasl_authenticator=authenticator,
                          broker_version='.'.join(str(v) for v in kafka_version))
